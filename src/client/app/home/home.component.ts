dd47542ab7a57adf87bd9a392d2866499069cb47
gopshyam <gopal.shyam94@gmail.com>
2017-04-28 05:41:50 -0700

    To-do list

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NameListService } from '../shared/name-list/name-list.service';
+import { TodoItem } from './todoitem';
 
 /**
  * This class represents the lazy loaded HomeComponent.
@@ -15,6 +16,7 @@ export class HomeComponent implements OnInit {
   newName: string = '';
   errorMessage: string;
   names: any[] = [];
+  todos: Array<TodoItem>;
 
   /**
    * Creates an instance of the HomeComponent with the injected
@@ -29,6 +31,7 @@ export class HomeComponent implements OnInit {
    */
   ngOnInit() {
     this.getNames();
+    this.todos = new Array<TodoItem>();
   }
 
   /**
@@ -37,9 +40,10 @@ export class HomeComponent implements OnInit {
   getNames() {
     this.nameListService.get()
       .subscribe(
-        names => this.names = names,
+        names => this.populateTodoList(names),
         error => this.errorMessage = <any>error
       );
+    console.log(this.names)
   }
 
   /**
@@ -48,9 +52,25 @@ export class HomeComponent implements OnInit {
    */
   addName(): boolean {
     // TODO: implement nameListService.post
-    this.names.push(this.newName);
+    this.todos.push(new TodoItem(this.newName, false));
     this.newName = '';
     return false;
+  }
+
+  setCompleted(todo: TodoItem, checked: boolean) {
+    todo.done = checked;
+  }
+
+  removeTodo(todo: TodoItem) {
+    this.todos.splice(this.todos.indexOf(todo), 1)
+  }
+
+  populateTodoList(names: string[]) {
+    console.log(names)
+    this.todos = new Array<TodoItem>();
+    for (var name of names) {
+      this.todos.push(new TodoItem(name, false));
+    }
   }
 
 }
